1ff03662f30c61cbd6dba5850be7460c24957188
andre cantarino <cantarino.andre@gmail.com>
2022-02-20 10:39:06 -0300

    extend styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 import BasicTitle from "./components/BasicTitle";
 import { DefaultButton, HipsterButton } from "./components/Buttons";
+import { Button } from "@material-ui/core";
+
+const StyledBtn = styled(Button)`
+  text-transform: capitalize;
+`
 
 function App() {
   return (
@@ -9,6 +14,8 @@ function App() {
       <BasicTitle special>Basic Title special</BasicTitle>
       <DefaultButton>Default button</DefaultButton>
       <HipsterButton>Hipster button</HipsterButton>
+      <Button color='primary' variant='contained'>Material button</Button>
+      <StyledBtn color='primary' variant='contained'>Styled Material button</StyledBtn>
     </div>
   );
 }
