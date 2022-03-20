1caf26241a363dfad7976144d0ab12c8f9a44c00
andre cantarino <cantarino.andre@gmail.com>
2022-03-20 11:26:52 -0300

    set theme with ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import Card from "./components/Card";
+import { useState } from "react";
+
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "./global-styles";
 
+const BaseTheme = {
+  color: '#222',
+  background: '#fff',
+};
+
+const DarkTheme = {
+  color: '#fff',
+  background: '#222',
+}
+
+const Container = styled.div`
+  padding: 2rem;
+  color: ${(props) => props.theme.color};
+  background: ${(props) => props.theme.background};
+`
+
 function App() {
+  const [baseTheme, setTheme] = useState(true);
+  const toggleTheme = () => setTheme(!baseTheme);
+
   return (
-    <div style={{ padding: '2rem' }}>
+    <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
       <GlobalStyles />
-      <Card />
-    </div>
+      <Container>
+        <h1>Hello World</h1>
+        <button className="btn" onClick={toggleTheme}>toggle me</button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
