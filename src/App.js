6b936fdb9ffcdf31a5bd435ca707d9cf0e9185ed
andre cantarino <cantarino.andre@gmail.com>
2022-03-20 12:28:14 -0300

    set button as a link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import GlobalStyles from "./global-styles";
-import Loading from "./components/Loading";
+import  { HipsterButton } from './components/Buttons';
 
 function App() {
   return (
-    <div styles={{padding: "2rem"}}>
+    <div style={{padding: "2rem"}}>
       <GlobalStyles />
-      <Loading />
+      <HipsterButton>Click me</HipsterButton>
+      <HipsterButton as="a" href="https://www.johnsmilga.com/">Click me</HipsterButton>
+      <HipsterButton>Click me</HipsterButton>
     </div>
   );
 }
